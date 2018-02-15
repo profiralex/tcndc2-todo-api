918eb7644117887a64bcf44c8a3bf7c3e6c954ea
Profir Alexandr <profiralex@gmail.com>
2018-02-15 21:33:01 +0200

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { User } = require('./models/user');
@@ -35,6 +36,23 @@ app.get('/todos', (req, res) => {
     .catch(e => {
       res.status(500).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(e => res.status(400).send());
 });
 
 app.listen(3000, () => {
