f79ff256ade3ca1f22c89791dc09b7513e625f2a
Profir Alexandr <profiralex@gmail.com>
2018-02-17 13:52:00 +0200

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -100,6 +100,21 @@ app.patch('/todos/:id', (req, res) => {
     .catch(e => res.status(400).send());
 });
 
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+
+  user
+    .save()
+    .then(() => user.generateAuthToken())
+    .then(token => {
+      res.header('x-auth', token).send({ user });
+    })
+    .catch(error => {
+      res.status(400).send({ error });
+    });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
