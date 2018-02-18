ece60a86790a623a0de57ab54d6430b6fdff3662
Profir Alexandr <profiralex@gmail.com>
2018-02-18 14:03:20 +0200

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -255,3 +255,16 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', () => {
+    return request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .then(() => User.findById(users[0]._id))
+      .then(user => {
+        expect(user.tokens.length).toBe(0);
+      });
+  });
+});
